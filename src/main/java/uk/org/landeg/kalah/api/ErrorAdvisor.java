c6ee61d9c39b81d52b5822dea66cd17c4177ffcc
Andrew Landeg <andys.junk.76@gmail.com>
2019-08-26 17:24:16 +0100

    http status for server errors

--- src/main/java/uk/org/landeg/kalah/api/ErrorAdvisor.java
+++ src/main/java/uk/org/landeg/kalah/api/ErrorAdvisor.java
@@ -1,5 +1,7 @@
 package uk.org.landeg.kalah.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import uk.org.landeg.kalah.api.model.ErrorResponse;
 import uk.org.landeg.kalah.exception.KalahClientException;
+import uk.org.landeg.kalah.exception.KalahException;
 import uk.org.landeg.kalah.exception.KalahGameNotFoundException;
 
 @RestControllerAdvice
 public class ErrorAdvisor {
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@ResponseStatus(code=HttpStatus.NOT_FOUND)
 	@ExceptionHandler(KalahGameNotFoundException.class)
 	public ErrorResponse handleKalahClientException(KalahGameNotFoundException ex) {
@@ -21,5 +26,18 @@ public class ErrorAdvisor {
 	@ExceptionHandler(KalahClientException.class)
 	public ErrorResponse handleKalahClientException(KalahClientException ex) {
 		return new ErrorResponse(ex);
+	}
+
+	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(KalahException.class)
+	public ErrorResponse handleKalahException(KalahException ex) {
+		log.error("SERVER ERROR", ex);
+		return new ErrorResponse(ex);
+	}
+
+	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	public ErrorResponse handleOtherException(Exception ex) {
+		return new ErrorResponse(new KalahException(ex.getMessage(), "SERVER_ERROR"));
 	}
 }
